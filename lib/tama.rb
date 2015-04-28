5112742e9083d68337be13103bae224cc05dcbaa
jessica engel & Cory Ribson <student@epicodus.com>
2015-04-28 16:30:51 -0700

    add methods to play with, and let your pet sleep.  Also methods to check in on the vitals of your pet, as well as a way to kill your pet.

--- lib/tama.rb
+++ lib/tama.rb
@@ -1,9 +1,12 @@
 class Tamagotchi
+
   define_method(:initialize) do |name|
     @name = name
     @food_level = 10
     @sleep_level = 10
     @activity_level = 10
+    @start_time = Time.new()
+    @check_in_time=@start_time
   end
 
   define_method(:name) do
@@ -20,6 +23,94 @@ class Tamagotchi
 
   define_method(:activity_level) do
     @activity_level
+  end
+
+  define_method(:age) do
+    age = Time.new() - @start_time
+    days = 0
+    minutes = 0
+    seconds = 0
+    until age.floor.==(0)
+      days = age./(86400).floor
+      age = age%86400
+      minutes = age./(60).floor
+      age = age%60
+      seconds = age.floor
+      age = age%1
+    end
+    age = "Your Tamagotchi has been alive for: #{days} days, #{minutes} minutes, and #{seconds} seconds"
+  end
+
+  define_method(:feed) do
+    if @food_level<10
+      @food_level+=1
+      @check_in_time=Time.new()
+      @name.concat(" was so hungry! Thanks for feeding them! Your food level is #{@food_level}")
+    else
+    end
+  end
+
+  define_method(:rest) do
+    if @sleep_level<10
+      @sleep_level+=1
+      @check_in_time=Time.new()
+      @name.concat(" was so tired! Thanks for letting them rest! Your sleep level is #{@sleep_level}")
+    else
+    end
+  end
+
+  define_method(:run_around) do
+    if @food_level<10
+      @food_level+=1
+      @check_in_time=Time.new()
+      @name.concat(" was getting unhappy! Thanks for playing with them! Your activity level is #{@activity_level}")
+    else
+    end
+  end
+
+  define_method(:check_in) do
+    time_since_check_in = Time.new() - @check_in_time
+    if time_since_check_in >= 180
+      self.decrement(10)
+    elsif time_since_check_in >= 162
+      self.decrement(9)
+    elsif time_since_check_in >= 144
+      self.decrement(8)
+    elsif time_since_check_in >= 126
+      self.decrement(7)
+    elsif time_since_check_in >= 108
+      self.decrement(6)
+    elsif time_since_check_in >= 90
+      self.decrement(5)
+    elsif time_since_check_in >= 72
+      self.decrement(4)
+    elsif time_since_check_in >= 54
+      self.decrement(3)
+    elsif time_since_check_in >= 36
+      self.decrement(2)
+    elsif time_since_check_in >= 18
+      self.decrement(1)
+    end
+  end
+
+  define_method(:alive?) do
+    if @food_level.<(1) || @sleep_level.<(1)
+      "You killed ".concat(@name).concat("!")
+    else
+      "Your pet is still alive!!!.....for now.\n
+      food level: #{@food_level} \n
+      @sleep_level: #{@sleep_level}"
+    end
+  end
+
+  define_method(:decrement) do |time|
+    @food_level-=time
+    @sleep_level-=time
+    @activity_level-=time
+  end
+
+  define_method(:kill) do
+    @food_level = 0
   end
 
 end
--- spec/tama_spec.rb
+++ spec/tama_spec.rb
@@ -12,4 +12,37 @@ describe(Tamagotchi) do
     end
   end
 
+  describe("#age") do
+    it("returns how long your tamagotchi has been alive") do
+      my_pet = Tamagotchi.new("buddy")
+      sleep(1)
+      expect(my_pet.age()).to eq("Your Tamagotchi has been alive for: 0 days, 0 minutes, and 1 seconds")
+    end
   end
+
+  describe(".check_in") do
+    it("depletes food, sleep, and activity based on time passed") do
+      my_pet = Tamagotchi.new("pleasework")
+      sleep(18)
+      my_pet.check_in()
+      expect(my_pet.food_level()).to(eq(9))
+    end
+  end
+
+  describe(".alive?") do
+    it("checks to see if your pet is still alive, if it is it outputs the current vitals") do
+      my_pet = Tamagotchi.new("Hobbes")
+      expect(my_pet.alive?()).to(eq("Your pet is still alive!!!.....for now.\n
+      food level: 10 \n
+      @sleep_level: 10"))
+    end
+  end
+
+  describe(":kill") do
+    it("kills them!") do
+      my_pet = Tamagotchi.new("EvilBaby")
+      my_pet.kill()
+      expect(my_pet.alive?()).to(eq("You killed EvilBaby!"))
+    end
+  end
+end
